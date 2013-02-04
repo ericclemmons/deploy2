e927af1f5cd0885ff7b973bbffedded1551b5785
Eric Clemmons <eric@smarterspam.com>
2013-02-04 14:21:52 -0600

    Initial genesis-skeleton

---
+++ README.md
@@ -0,0 +1,10 @@
+Deploy2 [![Build Status](https://travis-ci.org/ericclemmons/deploy2.png)](https://travis-ci.org/ericclemmons/deploy2)
+=======
+
+
+
+
+License
+-------
+
+MIT
---
+++ src/client/app/js/app.js
@@ -0,0 +1,25 @@
+angular
+  .module('app', [
+    'app.controllers',
+    'app.directives',
+    'app.templates'
+  ])
+  .config(['$locationProvider', '$routeProvider', function($location, $router) {
+    $router
+      .when('/', {
+        redirectTo: '/home'
+      })
+      .when('/home', {
+        templateUrl: 'app/partials/home.html'
+      })
+      .when('/guide', {
+        controller: 'app.controllers.guide',
+        templateUrl: 'app/partials/guide.html'
+      })
+      .otherwise({
+        controller: 'app.controllers.error',
+        templateUrl: 'app/partials/error/index.html'
+      })
+    ;
+  }])
+;
