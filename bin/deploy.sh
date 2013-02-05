c9b4328a1effa362b2fae840b664b7199c25a1c3
Eric Clemmons <eric@smarterspam.com>
2013-02-05 16:30:07 -0600

    Clean up deploy.sh

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -1,18 +1,39 @@
 #!/bin/bash
 
-REPO=$1
-BRANCH=$2
-COMMIT=$3
-TMP="${TMPDIR}./deploy2/${REPO}"
-
-if [ ! -d "${TMP}/.git" ] ; then
-    mkdir -p $TMP
-    git clone --depth=100 --branch=$BRANCH "git@github.com:${REPO}.git" $TMP
+# Help screen
+if [ -z "$1" -o -z "$2" -o -z "$3" ] ; then
+    echo "Usage: deploy.sh Username/Repo master COMMITTISH"
+    exit 1
 fi
 
-cd $TMP
+# Force develop/master
+if [ ! \( "develop" = "$2" -o "master" = "$2" \) ] ; then
+    echo "$2 is not a deployable branch."
+    exit 1
+fi
 
-git fetch origin
-git checkout -qf $COMMIT
+# Setup repository for deployment
+setup ()
+{
+    local TMP="${TMPDIR}/tmp/deploy2/$1"
 
-cap staging deploy --set branch=$BRANCH
+    if [ ! -d "${TMP}/.git" ] ; then
+        mkdir -p $TMP
+        git clone --depth=100 --branch=$2 "git@github.com:$1.git" $TMP
+    fi
+
+    cd $TMP
+
+    git fetch origin
+    git checkout -qf $3
+}
+
+# Custom deployment script
+deploy ()
+{
+    cap staging deploy --set branch=$2
+}
+
+setup $@ && deploy $@
+
+exit $?
